2e480064140166506c4493e152b465a53c87594b
Aman Gupta <4programmimg@gmail.com>
2024-05-06 21:41:58 +0530

    init commit of sidebar ai assitant

---
+++ extension/app-background.js
@@ -0,0 +1,3 @@
+// This is a firefox browser extension which displays results side by side on any search engine page whenever users searches something on the web from LLM like GPT3.5 by openai using users api key provided by themselves
+
+console.log("This is a popup!")
